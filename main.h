1c018b3f1e35a528a0c4a537e76507741fb61d55
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-06-25 01:48:32 +0000

    Header file

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*C standard library header file*/
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stddef.h>
+
+/*prototypes*/
+int _printf(const char *format, ...);
+#endif
