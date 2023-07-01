5415e6120e542ff2f27a899c23f8f84c10b85b05
kariukikinyanjui <michaelkariuki7@gmail.com>
2023-07-01 01:04:16 +0000

    Function update

--- handlers2.c
+++ handlers2.c
@@ -20,3 +20,37 @@ int handle_unsigned_int(va_list arg)
 
 	return (count);
 }
+/**
+ * handle_binary - a function to handle operations for printing binary
+ * @arg: argument to print
+ * Return: number of characters printed
+ */
+int handle_binary(va_list arg)
+{	
+	int j, i = 0;
+	unsigned int num = va_arg(arg, unsigned int);
+	int count = 0;
+	int binary[32];
+
+	if (num == 0)
+	{
+		_putchar('0');
+		count++;
+	}
+	else
+	{
+
+		while (num > 0)
+		{
+			binary[i] = num % 2;
+			num = num / 2;
+			i++;
+		}
+		for (i - 1; i >= 0; j--)
+		{
+			_putchar(binary[j] + '0');
+			count++;
+		}
+	}
+	return (count);
+}
